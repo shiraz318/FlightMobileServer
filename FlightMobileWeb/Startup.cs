5b7f5e88380f21a28b3607f9575686c90804398e
shiraz berger <shiraz422@gmail.com>
2020-06-25 12:50:13 +0300

    Final

--- FlightMobileWeb/Startup.cs
+++ FlightMobileWeb/Startup.cs
@@ -17,7 +17,8 @@ namespace FlightMobileAppServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the
+        // container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -25,7 +26,8 @@ namespace FlightMobileAppServer
             services.Configure<ServerData>(Configuration.GetSection("ServerData"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP
+        // request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
